e0714a19559723d0e37809e407021ec21fdc6fba
rafaelcf00 <rafacesar0070@gmail.com>
2024-03-20 09:26:21 -0300

    added pixel

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,23 @@ const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 export default function RootLayout({ children }) {
   const serviceModal = useServiceModal();
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '268896668933653');
+              fbq('track', 'PageView');
+            `}
+        </Script>
+      </head>
       <body className={poppins.className}>
         <ServiceModal
           isOpen={serviceModal.isOpen}
