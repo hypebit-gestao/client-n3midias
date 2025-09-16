239c2e1a45ac19127a262f4561b1e0894d5c03af
gestao-hypebit <gestao@hypebit.com.br>
2025-09-16 20:44:22 -0300

    updates

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/n3clinicas',     // URL que o usuário acessa
+        destination: 'https://inlead.digital/n3clinica/', // URL de destino
+        permanent: true,           // true = redirecionamento 301, false = 302
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
